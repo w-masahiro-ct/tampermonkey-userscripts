964f1b3a398e73793b411e0c21b280871c13cea2
Masahiro <watanabe@cadenza-tech.com>
2025-02-26 02:43:56 +0900

    feat: concentration.user.jsを追加

---
+++ src/concentration.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Concentration
+// @namespace    https://github.com/w-masahiro-ct/tampermonkey-userscripts
+// @version      1.0
+// @description  Concentration
+// @author       M
+// @match        *://www.youtube.com/*
+// @match        *://www.nicovideo.jp/*
+// @match        *://twitter.com/*
+// @match        *://x.com/*
+// @match        *://news.yahoo.co.jp/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @updateURL    https://github.com/w-masahiro-ct/tampermonkey-userscripts/raw/main/src/concentration.user.js
+// @downloadURL  https://github.com/w-masahiro-ct/tampermonkey-userscripts/raw/main/src/concentration.user.js
+// @supportURL   https://github.com/w-masahiro-ct/tampermonkey-userscripts
+// ==/UserScript==
+
+(() => {
+  'use strict';
+  location.href = 'https://www.google.com/';
+})();
